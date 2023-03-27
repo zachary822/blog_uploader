7b7b5e6b5a625b8832930ab6bc2a867fb13f0762
Zachary Juang <zachary822@me.com>
2023-03-27 00:52:33 -0400

    Better way to access data file

--- blog_uploader/create_post.py
+++ blog_uploader/create_post.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from importlib import resources
 
 import yaml
 from jinja2 import Template
@@ -15,11 +15,11 @@ def object_id_representer(dumper: yaml.Dumper, data: ObjectId) -> yaml.Node:
 yaml.add_representer(ObjectId, object_id_representer)
 
 
-TEMPLATE_PATH = Path(__file__).parent / "blog.md.jinja"
+TEMPLATE_PATH = resources.files("blog_uploader") / "blog.md.jinja"
 
 
 def create_post(title: str) -> str:
-    with open(TEMPLATE_PATH) as f:
+    with resources.as_file(TEMPLATE_PATH) as p, p.open("r") as f:
         template = Template(f.read())
     metadata = Metadata(id=ObjectId())
 
